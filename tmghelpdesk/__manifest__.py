d40b47fd20de432a568db0a8ab3fb0869cce59bf
Jonas Temple <jtemple@themagnetgroup.com>
2018-11-28 14:07:31 -0600

    TMG extensions of base Helpdesk

---
+++ tmghelpdesk/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "/home/odoo/mods/helpdesk",
+
+    'summary': """
+        Extends the Odoo Helpdesk module to check for unassigned tickets.""",
+
+    'sequence' : 2,
+
+    'description': """
+        This TMG module checks for urgent tickets that have not been assigned within 15 minutes.
+        If the ticket is not assigned a wider audience will be notified.
+    """,
+
+    'author': "The Magnet Group",
+    'website': "http://www.themagnetgroup.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Helpdesk',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['helpdesk'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
