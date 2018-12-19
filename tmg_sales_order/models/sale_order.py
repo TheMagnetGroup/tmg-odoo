2c9428bc60aaa33ab97c2ab9fe3106622fd9498b
ChristianDunn <cdunn@magnetllc.com>
2018-12-19 14:28:19 -0600

    =changed sales_order_notes to a html field

--- tmg_sales_order/models/sale_order.py
+++ tmg_sales_order/models/sale_order.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 
 class tmg_sales_order(models.Model):
     _inherit = "sale.order"
-    order_notes = fields.Text('Order Notes')
+    order_notes = fields.Html('Order Notes')
     production_ids = fields.One2many("mrp.production","sale_line_order_id", string="Production")
     production_count = fields.Integer(compute="_get_production_count")
 
