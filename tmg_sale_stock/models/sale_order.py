7fa145ccd40df854a234a9ee12e065e654f4e9b4
Emmie He <ehe@odoo.com>
2019-02-01 09:59:08 -0800

    [ADD] a somewhat working import feature for delivery addresses

--- tmg_sale_stock/models/sale_order.py
+++ tmg_sale_stock/models/sale_order.py
@@ -2,6 +2,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, Warning
 from odoo.addons import decimal_precision as dp
+from odoo.tools import float_compare, float_round
 
 
 class SaleOrderLineDelivery(models.Model):
@@ -24,20 +25,37 @@ class SaleOrderLineDelivery(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    xname = fields.Char('External ID', compute='_compute_sale_line_xname', store=False)
+
     delivery_ids = fields.One2many('sale.order.line.delivery', 'sale_line_id', string='Additional Deliveries (SOL)', copy=True)
 
     delivery_qty_sum = fields.Float('Delivery Qty Sum', compute='_compute_delivery_qty_sum', store=True,
                                     help='Technical field used to detect if delivery qty on SOL is exceeding current SOL qty',
                                     digits=dp.get_precision('Product Unit of Measure'))
 
+    # todo: not sure I like this...
+    @api.multi
+    def _compute_sale_line_xname(self):
+        for sol in self:
+            xid = self.env['ir.model.data'].sudo().search([('model', '=', 'sale.order.line'), ('res_id', '=', sol.id)], limit=1)
+            if xid:
+                sol.xname = xid.complete_name
+
     @api.multi
     @api.depends('delivery_ids', 'delivery_ids.qty')
     def _compute_delivery_qty_sum(self):
         for sol in self:
             sol.delivery_qty_sum = sum(sol.delivery_ids.mapped('qty'))
-            # todo: this is ugly, fix it emmie
-            if sol.delivery_qty_sum > sol.product_uom_qty:
-                sol.product_uom_qty = sol.delivery_qty_sum
+
+    # inherit the private _write here to capture the value change in depends
+    @api.multi
+    def _write(self, values):
+        res = super(SaleOrderLine, self)._write(values)
+        if values.get('product_uom_qty') or values.get('delivery_qty_sum'):
+            for sol in self:
+                if sol.product_uom_qty < sol.delivery_qty_sum:
+                    sol.product_uom_qty = sol.delivery_qty_sum
+        return res
 
     # onchange is too annoying for now, muted it
     # @api.multi
