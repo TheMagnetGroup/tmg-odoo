7b68f45c442f3a0cbfb50338d43fc43ce66d0a9c
the1aperson <32710917+the1aperson@users.noreply.github.com>
2019-02-15 17:24:02 -0600

    Add files via upload

---
+++ tmg_helpdesk_mods/models/helpdesk_ticket.py
@@ -0,0 +1,46 @@
+    # -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from datetime import datetime, date, timedelta
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    # Method to called by CRON to update SLA & statistics
+    @api.model
+    def tmg_check_sla(self):
+        tickets = self.search([('stage_id.is_close', '=', False),('sla_id','!=',False),('team_id.name','=','Urgent')])
+        urgent_channel = self.env['mail.channel'].search([('name', '=', 'urgent')])
+        for ticket in tickets:
+            if ticket.stage_id.sequence < ticket.sla_id.stage_id.sequence and fields.datetime.now() > ticket.deadline:
+                if urgent_channel:
+                    ticket.message_subscribe(channel_ids=[urgent_channel.id])
+                    urgent_channel.message_post(body=_('Urgent ticket %s was not assigned according to SLA!') % ticket.name, message_type='comment', subtype='mail.mt_comment')
+
+        return True
+
+    # Method to change Ticket Status to Assigned when Tech is added
+    @api.model
+    def create(self, vals):
+        if vals.get('user_id'):
+            vals['stage_id'] = 'In Progress'
+        return super(HelpdeskTicket, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        if vals.get('user_id'):
+            vals['stage_id'] = 1
+        return super(HelpdeskTicket, self).write(vals)
+
+    @api.onchange('user_id')
+    def tmg_assign_tech(self):
+        self.ensure_one()
+        for ticket in self:
+            if ticket.user_id:
+                ticket.stage_id = 1
+
+
+
+
+
