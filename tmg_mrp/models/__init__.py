39debe1f59b706047257578a7535c94aeeef5b92
ChristianDunn <cdunn@magnetllc.com>
2019-03-13 15:13:28 -0500

    Testing Dev

---
+++ tmg_mrp/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import stock_rule
+from . import mrp_production
+from . import stock_move
+from . import sale_order
---
+++ tmg_mrp/models/sale_order.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _, exceptions
+
+
+# We don't really do anything to sale order,
+# We use this model only in order to set a breakpoint and debug
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    order_notes = fields.Html('Order Notes')
+    production_ids = fields.One2many("mrp.production", "sale_line_order_id", string="Production Orders")
+    production_count = fields.Integer(compute="_get_production_count")
+
+    @api.depends('order_line')
+    def _get_production_count(self):
+        if self.production_ids:
+            self.production_count = len(self.production_ids)
+        else:
+            print("NotFound")
+#     # what happens when you confirm a sale order?
+#     def action_confirm(self):
+#         res = super(SaleOrder, self).action_confirm()
+#         return res
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+    production_order = fields.One2many('mrp.production', "sale_line_id")
