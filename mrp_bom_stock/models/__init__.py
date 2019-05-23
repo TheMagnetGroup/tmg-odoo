5c9bf05a9bc947a1719c4958b37e3e4879b1598f
ChristianDunn <cdunn@magnetllc.com>
2019-05-23 09:03:31 -0500

    Rebased to staging

---
+++ mrp_bom_stock/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import product
+from . import mrp_bom
+from . import sale
+from . import stock_location
---
+++ mrp_bom_stock/report/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import report_stock_kit
+from . import report_stock_kit_forcast
