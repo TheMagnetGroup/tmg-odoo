9156e2ef02d623d72c65612d3db7c392ce83472a
ChristianDunn <cdunn@magnetllc.com>
2019-07-19 09:03:30 -0500

    Changed commitment_date label to Ship Date on the Sale.Order

--- tmg_so_inhands/models/tmg_so_inhands.py
+++ tmg_so_inhands/models/tmg_so_inhands.py
@@ -6,6 +6,11 @@ class tmg_so_inhands(models.Model):
 
     _inherit = "sale.order"
     in_hands = fields.Date(string="In-Hands Date")
+    commitment_date = fields.Datetime('Ship Date',
+                                      states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+                                      copy=False, oldname='requested_date', readonly=True,
+                                      help="This is the delivery date promised to the customer. If set, the delivery order "
+                                           "will be scheduled based on this date rather than product lead times.")
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
