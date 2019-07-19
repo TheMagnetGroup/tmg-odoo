70d130e03b2a9dea0ac42ece39d51ff214aedadd
ChristianDunn <cdunn@magnetllc.com>
2019-07-19 09:59:10 -0500

    Added warnings for invalid dates

--- tmg_so_inhands/models/tmg_do_inhands.py
+++ tmg_so_inhands/models/tmg_do_inhands.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class tmg_do_inhands(models.Model):
 
     _inherit = "stock.picking"
-    in_hands = fields.Date(string="In-Hands Date", related="sale_id.in_hands")
+    in_hands = fields.Datetime(string="In-Hands Date", related="sale_id.in_hands")
