87c0ae52e275cc580853a2857683f56695c7fbad
bcowden-tmg <bcowden@themagnetgroup.com>
2019-07-26 11:52:24 -0400

    removed security folder

--- tmg_customer/__manifest__.py
+++ tmg_customer/__manifest__.py
@@ -1,30 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "tmg_salesperson",
+    'name': "tmg_customer",
 
     'summary': """
-        Changes label for existing "Salesperson" field""",
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Changes the label for the existing "Salesperson" field on the sales order object to "Customer Service Rep"
+        Long description of module's purpose
     """,
 
-    'author': "The Magnet Group",
-    'website': "http://www.themagnetgroup.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Sales',
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+    'depends': ['base'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
