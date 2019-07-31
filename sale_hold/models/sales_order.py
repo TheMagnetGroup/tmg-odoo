d4f43b1b9043deb1b2f03edb3d6c714b373e4484
ChristianDunn <cdunn@magnetllc.com>
2019-07-31 13:15:56 -0500

    Fixed bug with automation

--- sale_hold/models/sales_order.py
+++ sale_hold/models/sales_order.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
     on_hold = fields.Boolean(string='On Hold')
     approved_credit = fields.Boolean(string='Approved Credit', default=False)
     had_credit_hold = fields.Boolean(string="Had Credit Hold", default=False)
-    automated_hold = fields.Boolean(default=False)
+    is_automated_hold = fields.Boolean(default=False)
     def checkSecurity(self, value):
         hasGroup = False
         for grp in value.group_ids:
@@ -72,7 +72,7 @@ class SaleOrder(models.Model):
                     for hold in holds:
                         hasGroup = self.checkSecurity(hold)
                         if not hasGroup:
-                            if self.automated_hold and hold.credit_hold:
+                            if self.is_automated_hold and hold.credit_hold:
                                 nothold = True
                             else:
                                 raise Warning('Cannot add hold due to security on hold')
@@ -81,7 +81,7 @@ class SaleOrder(models.Model):
                         else:
                             note_list.append('Added Hold ' + hold.name)
                             #message_text = message_text + 'Added Hold ' + hold.name + ' <br/>'
-                    values.update({'automated_hold': False})
+                    values.update({'is_automated_hold': False})
             # if had_credit_hold:
             #     if not has_credit_hold:
             #         order.approved_credit = False
@@ -267,7 +267,7 @@ class SaleOrder(models.Model):
            # self.order_holds.write({''}) = holdsObj
 
             self.order_holds = self.order_holds | hold_ids
-            self.automated_hold = True
+            self.is_automated_hold = True
            # res = self.write({'order_holds': holdsObj})
             # return res
 
