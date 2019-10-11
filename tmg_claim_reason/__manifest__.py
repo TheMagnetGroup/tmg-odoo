c326c300f735bb9735ffb0c205221f5dac1a4829
ChristianDunn <cdunn@magnetllc.com>
2019-10-11 11:19:54 -0500

    Added security

--- tmg_claim_reason/__manifest__.py
+++ tmg_claim_reason/__manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/tmg_account_invoice_view.xml',
         'wizard/credit_note_claim.xml',
     ],
