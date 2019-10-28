60a10011d8cd2a66d4986cef6e2f820c0871b12c
ChristianDunn <cdunn@magnetllc.com>
2019-10-28 11:45:17 -0500

    Reorganized imports

--- tmg_import_delivery/models/__init__.py
+++ tmg_import_delivery/models/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-
+from . import fedex_delivery
 from . import stock_move
 from . import stock_picking
 from . import sale_order
-from . import fedex_delivery
 from . import delivery_order
