8447619ed4082336168756e8ac12bbd9641abbcf
jonbergt <jon_not_john1@hotmail.com>
2019-11-06 07:27:39 -0800

    Sales Team set as REQUIRED also on SALES ORDERS -- somehow model tmg_salesteam.py was missed on prior commit

--- tmg_salesteam/models/tmg_salesteam.py
+++ tmg_salesteam/models/tmg_salesteam.py
@@ -8,3 +8,8 @@ class tmg_salesteam(models.Model):
 
     team_member_ids = fields.Many2many('res.users', 'team_member_id_rel', 'member_id', 'user_id', string='Channel Team Members', domain= lambda self: [('groups_id', 'in', self.env.ref('base.group_user').id)], help="Add members to the sales team.")
 
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # provide link to partner/contact "is customer" flag to condition salesteam in sales.view_order_form
+    partner_is_customer = fields.Boolean('Is Customer', related='partner_id.customer')
