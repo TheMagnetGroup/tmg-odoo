ef8454d36b85735cbd5d3970fa00389776811062
bcowden <bcowden@themagnetgroup.com>
2019-11-20 13:06:08 -0500

    added many2Many_tags

--- tmg_attachment_types/models/tmg_attachment_types_models.py
+++ tmg_attachment_types/models/tmg_attachment_types_models.py
@@ -4,8 +4,7 @@ from odoo import models, fields, api
 
 
 class tmg_attachment_types(models.Model):
-    _inherit = 'ir.attachment'
+    _inherit = 'sale.order'
 
-    attachment_category = fields.Many2many('sale.order', help="Examples include Customer Art, Purchase Order, "
-                                                              "Proof, Production Art, etc..")
-
+    attachment_category = fields.Many2many('ir.attachment', attrs="{'readonly': [('user_id', '!=', 1)]}",
+                                           help="Tag the attachment with the appropriate category.")
