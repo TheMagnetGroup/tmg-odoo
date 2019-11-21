a1d26c812ee2c998ac9510bd24d2a9fe1151bae7
bcowden <bcowden@themagnetgroup.com>
2019-11-21 13:46:47 -0500

    Fix security error

--- tmg_attachment_types/__manifest__.py
+++ tmg_attachment_types/__manifest__.py
@@ -22,12 +22,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'security/tmg_attachment_types_security.xml',
+        # 'security/tmg_attachment_types_security.xml',
         'views/tmg_attachment_types_views.xml',
     ],
     # only loaded in demonstration mode
--- tmg_attachment_types/models/tmg_attachment_types_models.py
+++ tmg_attachment_types/models/tmg_attachment_types_models.py
@@ -6,5 +6,4 @@ from odoo import models, fields, api
 class tmg_attachment_types(models.Model):
     _inherit = 'sale.order'
 
-    attachment_category = fields.Many2many('ir.attachment', attrs="{'readonly': [('user_id', '!=', 1)]}",
-                                           help="Tag the attachment with the appropriate category.")
+    attachment_category = fields.Many2many('ir.attachment', help="Attachment Category Tags")
