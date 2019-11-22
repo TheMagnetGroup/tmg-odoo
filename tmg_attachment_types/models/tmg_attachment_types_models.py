6d30807a7389c299d24f34d6ca849df26bf410e8
bcowden <bcowden@themagnetgroup.com>
2019-11-22 10:45:41 -0500

    fix security errors

--- tmg_attachment_types/models/tmg_attachment_types_models.py
+++ tmg_attachment_types/models/tmg_attachment_types_models.py
@@ -6,4 +6,7 @@ from odoo import models, fields, api
 class tmg_attachment_types(models.Model):
     _inherit = 'sale.order'
 
+    class attachment_type(models.Model):
+        _name = 'attachment.type'
+
     attachment_category = fields.Many2many('ir.attachment', help="Attachment Category Tags")
