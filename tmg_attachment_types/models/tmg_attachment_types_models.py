7d05ffa0e96169c55fc75b78d45ff4cff6de704e
bcowden <bcowden@themagnetgroup.com>
2019-11-25 11:00:06 -0500

    renamed csv file ids

--- tmg_attachment_types/models/tmg_attachment_types_models.py
+++ tmg_attachment_types/models/tmg_attachment_types_models.py
@@ -4,6 +4,10 @@ from odoo import models, fields, api
 
 
 class tmg_attachment_types(models.Model):
-    _inherit = 'sale.order'
+    class tmg_attachment_types(models.Model):
+        _inherit = 'sale.order'
 
-    attachment_category = fields.Many2many('ir.attachment', help="Attachment Category Tags")
+        class attachment_type(models.Model):
+            _name = 'attachment.type'
+
+        attachment_category = fields.Many2many('ir.attachment', help="Attachment Category Tags", required=False)
