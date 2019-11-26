d423f4471e024c6936436e9ee0c13cc2d2dc17fa
bcowden <bcowden@themagnetgroup.com>
2019-11-26 17:12:42 -0500

    Fix errors

--- tmg_attachment_types/models/tmg_attachment_types_models.py
+++ tmg_attachment_types/models/tmg_attachment_types_models.py
@@ -4,16 +4,20 @@ from odoo import models, fields, api
 
 
 class tmg_attachment_types(models.Model):
-    _inherit = 'sale.order'
-    _description = "attachment types"
     _name = "attachment.type"
+    _description = "attachment types"
+
+    name = fields.Char(string="Name")
+    description = fields.Char(string="Description")
 
 
-name = fields.Char(string="Name")
-description = fields.Char(string="Description")
-
-
-class whatever(models.Model):
+class tmg_attachment(models.Model):
     _inherit = 'ir.attachment'
 
     attachment_category = fields.Many2many('attachment.type', help="Attachment Category Tags", required=False)
+
+
+class tmg_so_attachment(models.Model):
+    _inherit = 'sale.order'
+
+    attachments = fields.Many2many('ir.attachment', help="Attachments", required=False)
