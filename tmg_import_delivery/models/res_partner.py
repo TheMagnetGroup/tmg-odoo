715d6a9dc259efff5c2723f89dcfebba30f92bf7
ChristianDunn <cdunn@magnetllc.com>
2019-12-07 20:17:27 -0600

    Added pictures

--- tmg_import_delivery/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, exceptions
-from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
-
-class Partner(models.Model):
-    _inherit = "res.partner"
-    default_ups_third_party = fields.Char(string="UPS Account")
-    default_fedex_third_party = fields.Char(string="Fedex Account")
--- tmg_import_delivery/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import sale_order_line_delivery_entry_wizard
