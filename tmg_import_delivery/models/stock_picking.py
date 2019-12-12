c8b8d36b21dd81fffa64e77a1016bf437bf91541
ChristianDunn <cdunn@magnetllc.com>
2019-12-12 12:17:44 -0600

    Added Fedex to 3rd

--- tmg_import_delivery/models/stock_picking.py
+++ tmg_import_delivery/models/stock_picking.py
@@ -6,6 +6,9 @@ from odoo import models, fields
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
+    fedex_bill_my_account = fields.Boolean(related='carrier_id.fedex_bill_my_account', readonly=True)
     fedex_carrier_account = fields.Char(related='sale_id.fedex_carrier_account', string='Fedex Carrier Account', readonly=False)
-    ups_carrier_account = fields.Char(string='Carrier Account', readonly=False)
-    ups_service_type = fields.Selection( string="UPS Service Type", readonly=False)
+    ups_carrier_account = fields.Char(related='sale_id.ups_carrier_account', string='Carrier Account', readonly=False)
+    ups_service_type = fields.Selection(related='sale_id.ups_service_type', string="UPS Service Type", readonly=False)
+    fedex_service_type = fields.Selection(related='sale_id.fedex_service_type', string="Fedex Service Type")
+    ups_bill_my_account = fields.Boolean(related='carrier_id.ups_bill_my_account', readonly=True)
