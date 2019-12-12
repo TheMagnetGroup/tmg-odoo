09323b12f4f487f8ca918a9d3e9f48051fb4fb94
ChristianDunn <cdunn@magnetllc.com>
2019-12-12 13:51:18 -0600

    Major changes

--- tmg_import_delivery/models/stock_picking.py
+++ tmg_import_delivery/models/stock_picking.py
@@ -5,10 +5,22 @@ from odoo import models, fields
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    def _get_ups_service_types(self):
+        return self.env['delivery.carrier']._get_ups_service_types()
+    
     carrier_id = fields.Many2one(related='sale_id.carrier_id', string="Carrier", readonly=False)
     fedex_bill_my_account = fields.Boolean(related='carrier_id.fedex_bill_my_account', readonly=True)
-    fedex_carrier_account = fields.Char(related='sale_id.fedex_carrier_account', string='Fedex Carrier Account', readonly=False)
-    ups_carrier_account = fields.Char(related='sale_id.ups_carrier_account', string='Carrier Account', readonly=False)
-    ups_service_type = fields.Selection(related='sale_id.ups_service_type', string="UPS Service Type", readonly=False)
-    fedex_service_type = fields.Selection(related='sale_id.fedex_service_type', string="Fedex Service Type")
+    fedex_carrier_account = fields.Char(string='Fedex Carrier Account', readonly=False)
+    ups_carrier_account = fields.Char(string='UPS Carrier Account', readonly=False)
+    ups_service_type = fields.Selection(_get_ups_service_types, string="UPS Service Type")
+    fedex_service_type = fields.Selection([('INTERNATIONAL_ECONOMY', 'INTERNATIONAL_ECONOMY'),
+                                           ('INTERNATIONAL_PRIORITY', 'INTERNATIONAL_PRIORITY'),
+                                           ('FEDEX_GROUND', 'FEDEX_GROUND'),
+                                           ('FEDEX_2_DAY', 'FEDEX_2_DAY'),
+                                           ('FEDEX_2_DAY_AM', 'FEDEX_2_DAY_AM'),
+                                           ('FEDEX_3_DAY_FREIGHT', 'FEDEX_3_DAY_FREIGHT'),
+                                           ('FIRST_OVERNIGHT', 'FIRST_OVERNIGHT'),
+                                           ('PRIORITY_OVERNIGHT', 'PRIORITY_OVERNIGHT'),
+                                           ('STANDARD_OVERNIGHT', 'STANDARD_OVERNIGHT')], string="Fedex Service Type")
     ups_bill_my_account = fields.Boolean(related='carrier_id.ups_bill_my_account', readonly=True)
