61a2aa9632ad2c2822b4170529d998054761ea75
ChristianDunn <cdunn@magnetllc.com>
2019-12-12 12:43:06 -0600

    Added service type selections

--- tmg_import_delivery/models/sale_order.py
+++ tmg_import_delivery/models/sale_order.py
@@ -7,4 +7,14 @@ class SaleOrder(models.Model):
 
     fedex_carrier_account = fields.Char(string='FedEx Carrier Account', copy=False)
     fedex_bill_my_account = fields.Boolean(related='carrier_id.fedex_bill_my_account', readonly=True)
+    fedex_service_type = fields.Selection([('INTERNATIONAL_ECONOMY', 'INTERNATIONAL_ECONOMY'),
+                                           ('INTERNATIONAL_PRIORITY', 'INTERNATIONAL_PRIORITY'),
+                                           ('FEDEX_GROUND', 'FEDEX_GROUND'),
+                                           ('FEDEX_2_DAY', 'FEDEX_2_DAY'),
+                                           ('FEDEX_2_DAY_AM', 'FEDEX_2_DAY_AM'),
+                                           ('FEDEX_3_DAY_FREIGHT', 'FEDEX_3_DAY_FREIGHT'),
+                                           ('FIRST_OVERNIGHT', 'FIRST_OVERNIGHT'),
+                                           ('PRIORITY_OVERNIGHT', 'PRIORITY_OVERNIGHT'),
+                                           ('STANDARD_OVERNIGHT', 'STANDARD_OVERNIGHT')],
+                                          default='INTERNATIONAL_PRIORITY', string="Fedex Service Type")
 
