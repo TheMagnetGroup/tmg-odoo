c3bf0cfd7259708d720024e107fd9e24ae174f98
ChristianDunn <cdunn@magnetllc.com>
2019-12-12 12:56:23 -0600

    Added carier to related

--- tmg_import_delivery/models/stock_picking.py
+++ tmg_import_delivery/models/stock_picking.py
@@ -5,7 +5,7 @@ from odoo import models, fields
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
+    carrier_id = fields.Many2one(related='sale_id.carrier_id', string="Carrier", readonly=False)
     fedex_bill_my_account = fields.Boolean(related='carrier_id.fedex_bill_my_account', readonly=True)
     fedex_carrier_account = fields.Char(related='sale_id.fedex_carrier_account', string='Fedex Carrier Account', readonly=False)
     ups_carrier_account = fields.Char(related='sale_id.ups_carrier_account', string='Carrier Account', readonly=False)
