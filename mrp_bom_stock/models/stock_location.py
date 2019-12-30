a040a722b06ed7a07a76037c59aa9563a4feba76
Keyur Gajjar <kga@odoo.com>
2019-12-30 10:53:02 -0800

    [IMP] improved the way warehouse to a location is associated. Instead of using get_warehouse() which may possibly go into recursion.

--- mrp_bom_stock/models/stock_location.py
+++ mrp_bom_stock/models/stock_location.py
@@ -11,13 +11,56 @@ class StockLocation(models.Model):
     @api.model
     def create(self, vals):
         location = super(StockLocation, self).create(vals)
-        if location.location_id:
-            location.warehouse_id = location.get_warehouse()
+        if location.location_id and location.usage == 'internal':
+            wh = False
+            if location.location_id.warehouse_id:
+                wh = location.location_id.warehouse_id
+            else:
+                warehouses = self.env['stock.warehouse'].sudo().search([])
+                view_loc = False
+                loc = location
+                while not view_loc:
+                    if loc.usage == 'view':
+                        view_loc = loc
+                    if not location.location_id:
+                        break
+                    loc = location.location_id
+                if view_loc:
+                    wh = warehouses.filtered(lambda w: w.view_location_id == view_loc)
+            location.warehouse_id = wh
         return location
 
     @api.multi
     def write(self, vals):
         res = super(StockLocation, self).write(vals)
-        for location in self.filtered(lambda l: l.location_id and not l.warehouse_id):
-            location.warehouse_id = location.get_warehouse()
+        if 'location_id' in vals.keys():
+            for location in self.filtered(lambda l: l.location_id and l.usage == 'internal'):
+                wh = False
+                if location.location_id.warehouse_id:
+                    wh = location.location_id.warehouse_id
+                else:
+                    warehouses = self.env['stock.warehouse'].sudo().search([])
+                    view_loc = False
+                    loc = location
+                    while not view_loc:
+                        if loc.usage == 'view':
+                            view_loc = loc
+                        if not location.location_id:
+                            break
+                        loc = location.location_id
+                    if view_loc:
+                        wh = warehouses.filtered(lambda w: w.view_location_id == view_loc)
+                location.warehouse_id = wh
         return res
+
+
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    @api.model
+    def create(self, vals):
+        warehouse = super(StockWarehouse, self).create(vals)
+        sub_locations = self._get_locations_values(vals)
+        for loc_field in list(sub_locations.keys()) + ['view_location_id']:
+            warehouse[loc_field].warehouse_id = warehouse
+        return warehouse
