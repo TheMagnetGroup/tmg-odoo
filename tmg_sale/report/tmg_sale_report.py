aa6dcf49c2bb773583360d175523f18a2c20aeac
Jonas Temple <jtemple@themagnetgroup.com>
2020-01-23 16:58:26 -0600

    Added on hold and on production hold to the sales report. Enabled editing of commitment date even if the sale order has been confirmed.

--- tmg_sale/report/tmg_sale_report.py
+++ tmg_sale/report/tmg_sale_report.py
@@ -13,13 +13,17 @@ class SaleReport(models.Model):
     in_hands = fields.Date('In Hands Date', readonly=True)
     decoration_method = fields.Char('Decoration Method', readonly=True)
     commitment_date = fields.Date('Ship Date', readonly=True)
+    on_hold = fields.Boolean('On Hold', readonly=True)
+    on_production_hold = fields.Boolean('On Production Hold', readonly=True)
 
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['in_hands'] = ', s.in_hands as in_hands'
         fields['decoration_method'] = ', l.decoration_method as decoration_method'
         fields['commitment_date'] = ', s.commitment_date'
+        fields['on_hold'] = ', s.on_hold'
+        fields['on_production_hold'] = ', s.on_production_hold'
 
-        groupby += ', s.in_hands, l.decoration_method, s.commitment_date'
+        groupby += ', s.in_hands, l.decoration_method, s.commitment_date, s.on_hold, s.on_production_hold'
 
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
