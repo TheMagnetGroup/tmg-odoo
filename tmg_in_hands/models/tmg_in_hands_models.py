f64289285e1b36c4d181ca33f7a12bb4c89fa7b2
bcowden-tmg <bcowden@themagnetgroup.com>
2020-01-31 13:22:41 -0500

    delete

--- tmg_in_hands/models/tmg_in_hands_models.py
+++ tmg_in_hands/models/tmg_in_hands_models.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class tmg_in_hands(models.Model):
     _inherit = "sale.order"
 
-    in_hands = fields.Date('In Hands Date', readonly=True)
+    in_hands = fields.Date()
