4fe39cf5b0645410f90a3450180b04b3cf051eaa
bcowden-tmg <bcowden@themagnetgroup.com>
2020-02-03 10:47:43 -0500

    delete tmg_in_hands folder to combine with tmg_so_inhands

--- tmg_in_hands/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import tmg_in_hands_models
--- tmg_in_hands/models/tmg_in_hands_models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class tmg_in_hands(models.Model):
-    _inherit = "sale.order"
-
-    in_hands = fields.Date()
