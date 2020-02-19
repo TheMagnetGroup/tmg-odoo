b969e8cfe199823e099ed219e98c7fe984034773
bcowden-tmg <bcowden@themagnetgroup.com>
2020-02-19 13:52:46 -0500

    change loading sequence of mods

--- tmg_available_to_sell/__manifest__.py
+++ tmg_available_to_sell/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale', 'stock'],
+    'depends': ['sale', 'stock', 'mrp_bom_stock'],
 
     # always loaded
     'data': [
