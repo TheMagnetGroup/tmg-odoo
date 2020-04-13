59c9fdada327b6ac4dbb5949d7dc8654bd52bf4d
Jonas Temple <jtemple@themagnetgroup.com>
2020-04-13 17:40:31 -0500

    Initial commit

--- tmg_customer/models/tmg_customer_models.py
+++ tmg_customer/models/tmg_customer_models.py
@@ -3,6 +3,16 @@
 from odoo import models, fields, api
 
 
+# Buying group model
+class BuyingGroup(models.Model):
+    _name = 'partner.buying.group'
+    _description = 'Buying Groups'
+    _order = 'name'
+
+    name = fields.Char(string='Buying Group', required=True)
+    active = fields.Boolean(default=True, help="The active field allows you to hide the category without removing it.")
+
+
 class tmg_customer(models.Model):
 
     _inherit = 'res.partner'
