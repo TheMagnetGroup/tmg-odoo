db2b45ec0d49a282b0332b4977f3b94fada8eb52
Jonas Temple <jtemple@themagnetgroup.com>
2020-04-15 17:24:22 -0500

    Added buying group to order & invoice screens. Adding setting of buying group on orders and invoices. Added buying group to the sales and invoice analysis report.

--- tmg_customer/__manifest__.py
+++ tmg_customer/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/tmg_customer_views.xml',
        # 'views/templates.xml',
     ],
