2e13a9dfbca8dec2e6e1d4cfda81639d02531966
ChristianDunn <cdunn@magnetllc.com>
2020-05-29 14:37:21 -0500

    Initial Commit

---
+++ tmg_external_apis/models/APILogging.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+import json
+
+
+class APILogging(models.Model):
+    _name = 'tmg_external_api.api_logging'
+    api_name = fields.Char(string="API Name")
+    partner_id = fields.Many2one("res.partner", string= "partner")
+    request = fields.Text(string="Request")
