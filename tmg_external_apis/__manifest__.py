a8b5c2072e64fae3f63a513b9841badfd84150a5
ChristianDunn <cdunn@magnetllc.com>
2020-05-29 15:14:05 -0500

    Reverted manifest

--- tmg_external_apis/__manifest__.py
+++ tmg_external_apis/__manifest__.py
@@ -3,19 +3,20 @@
     'name': "TMG External API",
 
     'summary': """
-        Module to contain all APIs developed by TMG""",
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Creates a framework for logging, execution, and visibility for External APIs from The Magnet Group
+        Long description of module's purpose
     """,
     "application": True,
-    'author': "The Magnet Group",
-    'website': "http://www.themagnetgroup.com",
-
+    'author': "TheMagnetGroup",
+    'website': 'https://www.odoo.com',
+    'license': 'LGPL-3',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'sale',
+    'category': 'TMG External API',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
