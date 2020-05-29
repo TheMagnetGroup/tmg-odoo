a46ed403362971019ce0b6a80360395b61f19239
ChristianDunn <cdunn@magnetllc.com>
2020-05-29 15:17:27 -0500

    Changed manifest

--- tmg_external_apis/__manifest__.py
+++ tmg_external_apis/__manifest__.py
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'TMG External API',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-      'depends': ['tmg_so_inhands', 'tmg_sale','sales_margin_percentage'],
+    'depends': ['tmg_so_inhands', 'tmg_sale','sales_margin_percentage'],
 
     # always loaded
     'data': [
