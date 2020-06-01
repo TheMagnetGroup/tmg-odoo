247bb69c89f8d0c1c900f7c92b3ab524269e2c2a
ChristianDunn <cdunn@magnetllc.com>
2020-06-01 11:34:38 -0500

    Changes

--- tmg_external_api/models/APILogging.py
+++ tmg_external_api/models/APILogging.py
@@ -5,5 +5,5 @@ import json
 class APILogging(models.Model):
     _name = 'tmg_external_api.api_logging'
     api_name = fields.Char(string="API Name")
-    partner_id = fields.Many2one("res.partner", string= "partner")
+    partner_id = fields.Many2one("res.partner", string="partner")
     request = fields.Text(string="Request")
