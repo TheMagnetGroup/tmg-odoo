b0c778a5feb062b31663a1de38a8f1ec07e78c64
ChristianDunn <cdunn@magnetllc.com>
2020-06-02 10:39:34 -0500

    Changes per Jonas

--- tmg_external_api/models/APILogging.py
+++ tmg_external_api/models/APILogging.py
@@ -4,6 +4,12 @@ import json
 
 class APILogging(models.Model):
     _name = 'tmg_external_api.api_logging'
-    api_name = fields.Char(string="API Name")
-    partner_id = fields.Many2one("res.partner", string="partner")
+    name = fields.Char(string="Display Name", store=True, readonly=True, compute="_get_order_name")
+    api_name = fields.Char(string="API Name", required=True)
+    partner_id = fields.Many2one("res.partner", string="partner", required=True)
     request = fields.Text(string="Request")
+
+
+    @api.depends('api_name', 'partner_id', 'name')
+    def _get_order_name(self):
+        self.name= self.api_name + "," + self.partner_id.name
