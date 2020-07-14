03c7814c8d7fa824dee58dae50b48d9d99248501
Keyur Gajjar <kga@odoo.com>
2020-07-13 17:22:03 -0700

    [ADD] consider product virtual_available_qty instead of virtual available quanitity while raising popup for less available quantity. - Consider sum of product variants's sum of virtual available qty in case of it is less then the common product in the bill of materials. - On product an well as on product template display virtual_available_qty also in case off there isno bill of material.

--- mrp_bom_stock/models/sale.py
+++ mrp_bom_stock/models/sale.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
@@ -10,3 +11,35 @@ class SaleOrderLine(models.Model):
     def _check_routing(self):
         super(SaleOrderLine, self)._check_routing()
         return False
+
+    @api.onchange('product_uom_qty', 'product_uom', 'route_id')
+    def _onchange_product_id_check_availability(self):
+        if not self.product_id or not self.product_uom_qty or not self.product_uom:
+            self.product_packaging = False
+            return {}
+        if self.product_id.type == 'product':
+            precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+            product = self.product_id.with_context(
+                warehouse=self.order_id.warehouse_id.id,
+                lang=self.order_id.partner_id.lang or self.env.user.lang or 'en_US'
+            )
+            product_qty = self.product_uom._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
+            if float_compare(product.virtual_available_qty, product_qty, precision_digits=precision) == -1:
+                is_available = self._check_routing()
+                if not is_available:
+                    message =  _('You plan to sell %s %s of %s but you only have %s %s available in %s warehouse.') % \
+                            (self.product_uom_qty, self.product_uom.name, self.product_id.name, product.virtual_available_qty, product.uom_id.name, self.order_id.warehouse_id.name)
+                    # We check if some products are available in other warehouses.
+                    if float_compare(product.virtual_available_qty, self.product_id.virtual_available_qty, precision_digits=precision) == -1:
+                        message += _('\nThere are %s %s available across all warehouses.\n\n') % \
+                                (self.product_id.virtual_available, product.uom_id.name)
+                        for warehouse in self.env['stock.warehouse'].search([]):
+                            quantity = self.product_id.with_context(warehouse=warehouse.id).virtual_available
+                            if quantity > 0:
+                                message += "%s: %s %s\n" % (warehouse.name, quantity, self.product_id.uom_id.name)
+                    warning_mess = {
+                        'title': _('Not enough inventory!'),
+                        'message' : message
+                    }
+                    return {'warning': warning_mess}
+        return {}
