1773a754d9a3b1eed432d3510bf22b90d41f62ac
Jonas Temple <jtemple@themagnetgroup.com>
2020-11-04 17:47:26 -0600

    Initial commit

--- tmg_stock_extensions/__manifest__.py
+++ tmg_stock_extensions/__manifest__.py
@@ -6,6 +6,9 @@
 
     'description': """
         Extensions to models, views and functions of Stock and Delivery functions based on TMG
+        * Jonas Temple - 11/4/2020 - Added "Verify" function to inventory adjustment when no quantity change is
+          required. Also added "last verified" fields to the stock.inventory.line model.  Added code to set the 
+          inventory move dates with the accounting date if supplied.
     """,
 
     'author': "The Magnet Group",
@@ -26,7 +29,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/tmg_delivery_templates.xml'
+        'views/tmg_delivery_templates.xml',
+        'views/tmg_inventory_adjustment.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- tmg_stock_extensions/models/models.py
+++ tmg_stock_extensions/models/models.py
@@ -1,9 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     scheduled_date = fields.Datetime(string='Scheduled Date', store=True, related='picking_id.scheduled_date')
+
+class InventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+
+    last_verified_uid = fields.Many2one(comodel_name='res.users', string='Last Verified By')
+    last_verified_date = fields.Datetime(string='Last Verified On')
+
+    @api.multi
+    def verify(self):
+        for sil in self:
+            self.write({
+                'last_verified_uid': self.env.user.id,
+                'last_verified_date': datetime.today()
+            })
+
+    @api.multi
+    def write(self, vals):
+        vals['last_verified_uid'] = self.env.user.id
+        vals['last_verified_date'] = datetime.today()
+        res = super(InventoryLine, self).write(vals)
+        self._check_no_duplicate_line()
+        return res
+
+    def _generate_moves(self):
+        res = super(InventoryLine, self)._generate_moves()
+        for move in res:
+            if move.inventory_id.accounting_date:
+                super().write({'date': datetime.combine(move.inventory_id.accounting_date, datetime.min.time())})
+        return res
 
