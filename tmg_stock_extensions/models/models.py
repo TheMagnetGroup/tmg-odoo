244c3a4a5ed25f417af37f6ca5648d89634125b8
Jonas Temple <jtemple@themagnetgroup.com>
2020-11-05 14:08:04 -0600

    Finalized setting the date on inventory moves from the adjustment accounting date. Also added the last verified information on the tree view and report.

--- tmg_stock_extensions/models/models.py
+++ tmg_stock_extensions/models/models.py
@@ -3,16 +3,24 @@
 from odoo import models, fields, api
 from datetime import datetime
 
+
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     scheduled_date = fields.Datetime(string='Scheduled Date', store=True, related='picking_id.scheduled_date')
 
+
+class StockInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    accounting_date = fields.Date(copy=False)
+
+    
 class InventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
-    last_verified_uid = fields.Many2one(comodel_name='res.users', string='Last Verified By')
-    last_verified_date = fields.Datetime(string='Last Verified On')
+    last_verified_uid = fields.Many2one(comodel_name='res.users', string='Last Verified By', copy=False)
+    last_verified_date = fields.Datetime(string='Last Verified On', copy=False)
 
     @api.multi
     def verify(self):
@@ -30,10 +38,21 @@ class InventoryLine(models.Model):
         self._check_no_duplicate_line()
         return res
 
-    def _generate_moves(self):
-        res = super(InventoryLine, self)._generate_moves()
-        for move in res:
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _action_done(self):
+        moves_todo = super(StockMove, self)._action_done()
+
+        for move in moves_todo:
+            # If the accounting date was specified on the adjustment then set the stock.move and
+            # stock.move.line entries to the same date.
             if move.inventory_id.accounting_date:
-                super().write({'date': datetime.combine(move.inventory_id.accounting_date, datetime.min.time())})
-        return res
+                new_date = datetime.combine(move.inventory_id.accounting_date, datetime.max.time())
+                move.write({'date': new_date})
+                for move_line in move.move_line_ids:
+                    move_line.write({'date': new_date})
+
+        return moves_todo
 
