a821775020d1596e2f32f51e598b86316b3d2128
ChristianDunn <cdunn@magnetllc.com>
2020-11-20 14:09:23 -0600

    Added restriction on delete of technical contact

--- tmg_external_api/__manifest__.py
+++ tmg_external_api/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['tmg_sale',
