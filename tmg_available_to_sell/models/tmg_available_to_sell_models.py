cebed8333b6def0da83d688e3ebac47c30c24724
Jonas Temple <jtemple@themagnetgroup.com>
2021-01-20 08:56:29 -0600

    Changed the ala fields to stored and added a depends statement to the routine to recalculate the ala fields whenever a fixed price changes on the product's pricelist items. Also added search for product templates and variants for decoration method.

--- tmg_available_to_sell/models/tmg_available_to_sell_models.py
+++ tmg_available_to_sell/models/tmg_available_to_sell_models.py
@@ -5,43 +5,28 @@ from odoo.addons import decimal_precision as dp
 
 class tmg_available_to_sell(models.Model):
     _inherit = 'product.template'
-
-    # virtual_available_qty = fields.Float(
-    #     'Available Quantity', search='_search_virtual_available_qty',
-    #     digits=dp.get_precision('Product Unit of Measure'), compute='_compute_quantities',
-    #     help="Available Quantity (computed as Quantity On Hand "
-    #          "- Outgoing)\n")
     virtual_available_qty = fields.Float(
-        'Available Quantity', store=True,
+        'Available Quantity',
         digits=dp.get_precision('Product Unit of Measure'), compute='_compute_quantities',
         help="Available Quantity (computed as Quantity On Hand "
              "- Outgoing)\n")
+
     # def _compute_quantities(self):
     #     res = super(tmg_available_to_sell, self)._compute_quantities()
     #     for template in self:
     #         template.virtual_available_qty = template.qty_available - template.outgoing_qty
 
-    def _search_virtual_available_qty(self, operator, value):
-        return [('virtual_available_qty', operator, value)]
 
 class _compute_quantities(models.Model):
     _inherit = 'product.product'
-
-    # virtual_available_qty = fields.Float(
-    #     'Available Quantity', search='_search_virtual_available_qty',
-    #     digits=dp.get_precision('Product Unit of Measure'), compute='_compute_quantities',
-    #     help="Available Quantity (computed as Quantity On Hand "
-    #          "- Outgoing)\n")
     virtual_available_qty = fields.Float(
-        'Available Quantity', store=True,
+        'Available Quantity',
         digits=dp.get_precision('Product Unit of Measure'), compute='_compute_quantities',
         help="Available Quantity (computed as Quantity On Hand "
              "- Outgoing)\n")
+
     # @api.depends('stock_move_ids.product_qty', 'stock_move_ids.state')
     # def _compute_quantities(self):
     #     super(_compute_quantities, self)._compute_quantities()
     #     for product in self:
     #         product.virtual_available_qty = product.qty_available - product.outgoing_qty
-
-    def _search_virtual_available_qty(self, operator, value):
-        return [('virtual_available_qty', operator, value)]
