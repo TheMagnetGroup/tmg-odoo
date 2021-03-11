9534e5e7fc5f7b2f03beb48867d07326d36f4772
ChristianDunn <cdunn@magnetllc.com>
2021-03-11 10:28:35 -0600

    Fixed PO name

--- mrp_job/__manifest__.py
+++ mrp_job/__manifest__.py
@@ -8,6 +8,7 @@ Manufacturing JOB: Muliple MO processing
 
     * Christian Dunn - 2020/02/07 -   Added picking location to sale report.
     * Christian Dunn - 2020/04/23 -   Limited visibility of the printed button on Sale Order.
+    * Christian Dunn - 2021/03/11 -   Changes to MRP Report
 """,
     "author": "Odoo Inc",
     'website': "https://www.odoo.com",
