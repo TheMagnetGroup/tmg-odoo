45e192df77f63037aee14e3bd81f7c1e44ede116
ChristianDunn <cdunn@magnetllc.com>
2021-04-27 12:16:26 -0500

    Changed to Sale

--- tmg_proofing/__manifest__.py
+++ tmg_proofing/__manifest__.py
@@ -17,7 +17,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale','tmg_sale'],
+    'depends': ['tmg_sale'],
 
     # always loaded
     'data': [
