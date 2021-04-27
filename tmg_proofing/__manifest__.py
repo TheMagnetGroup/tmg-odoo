aa81213a57ee46da92ab03d7b778555276344dcb
ChristianDunn <cdunn@magnetllc.com>
2021-04-27 12:07:24 -0500

    Changed Category

--- tmg_proofing/__manifest__.py
+++ tmg_proofing/__manifest__.py
@@ -12,14 +12,12 @@
     'author': "The Magnet Group",
     'website': "http://www.themagnetgroup.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Sale',
+
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['tmg_sale'],
+    'depends': ['sale','tmg_sale'],
 
     # always loaded
     'data': [
