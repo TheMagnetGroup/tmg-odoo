eae3088c965fd9ae4ba58acdf0b3fc2d9e7889c7
ChristianDunn <cdunn@magnetllc.com>
2021-04-27 12:58:33 -0500

    Typo

--- tmg_proofing/__manifest__.py
+++ tmg_proofing/__manifest__.py
@@ -23,7 +23,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
