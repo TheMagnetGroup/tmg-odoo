72fe824db7c08b4c380a92b6a427128c57400771
ChristianDunn <cdunn@magnetllc.com>
2021-04-27 11:58:12 -0500

    Views

--- tmg_proofing/__manifest__.py
+++ tmg_proofing/__manifest__.py
@@ -3,24 +3,23 @@
     'name': "tmg_proofing",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Creates the concept of sending customer's proofs in Odoo """,
 
     'description': """
-        Long description of module's purpose
+        Proofs can be sent on the Sale Line to the customer to review to begin the proofing process
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "The Magnet Group",
+    'website': "http://www.themagnetgroup.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sale',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['tmg_sale'],
 
     # always loaded
     'data': [
