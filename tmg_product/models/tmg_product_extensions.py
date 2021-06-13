9929d93dd2b710206b1efb1c17e6c37f7de611ea
Jonas Temple <jtemple@themagnetgroup.com>
2021-06-12 20:03:53 -0500

    Dump sage response

--- tmg_product/models/tmg_product_extensions.py
+++ tmg_product/models/tmg_product_extensions.py
@@ -600,6 +600,8 @@ class ProductTemplate(models.Model):
                 export_error = True
                 export_message = sageresponsestr
 
+            print(sageresponsestr)
+
             # At this point if there were any errors we will update all export accounts with the same error since they apply
             # to the entire batch
             if export_error:
