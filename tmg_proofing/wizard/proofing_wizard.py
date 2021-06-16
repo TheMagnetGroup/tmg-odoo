d1bb6cbf18412740cd814ca31f8d6615ab179535
ChristianDunn <cdunn@magnetllc.com>
2021-06-16 15:54:10 -0500

    Major changes

--- tmg_proofing/wizard/proofing_wizard.py
+++ tmg_proofing/wizard/proofing_wizard.py
@@ -10,15 +10,14 @@ import re
 class SaleOrderLineSendProofWizard(models.TransientModel):
     _name = 'sale.order.line.send.proof.wizard'
     _description = 'sale order line proof entry wizard'
-
     # explicitly pass in context
+
     def _default_sol(self):
         return self.env['sale.order.line'].browse(self.env.context.get('active_ids'))[0]
     sale_line_id = fields.Many2one('sale.order.line', string='Active SOLs', ondelete='cascade', required=True, default=_default_sol)
     sale_order = fields.Many2one('sale.order', related='sale_line_id.order_id')
-    attachment_ids = fields.One2many('ir.attachment', compute='_compute_attachment_ids',
-                                      string="Main Attachments")
-
+    # attachment_ids = fields.One2many('ir.attachment', compute='_compute_attachment_ids',
+    #                                   string="Main Attachments")
     art_file = fields.Many2one("ir.attachment", string="ArtFiles",
                                domain="[('res_id','in',[sale_order]),('type', '=', 'url')]")
 
@@ -30,34 +29,28 @@ class SaleOrderLineSendProofWizard(models.TransientModel):
 
     def check_active_proof(self):
         proofs = any(l.state == 'pending' for l in self.sale_line_id.proofing_lines)
-
         if proofs:
             raise UserError('Please resolve pending proofs before creating new ones.')
 
-    def _compute_attachment_ids(self):
-        for order in self:
-            soID = self._default_sol().order_id
-            attachment_idss = self.env['ir.attachment'].search([('res_id', '=', soID.id), ('res_model', '=', 'sale.order')]).ids
-            message_attachment_ids = soID.mapped('message_ids.attachment_ids').ids  # from mail_thread
-            ids = list(set(attachment_idss) - set(message_attachment_ids))
-            attachmentObj = self.env['ir.attachment'].browse(ids)
-            self.attachment_ids = attachmentObj
-            return attachmentObj
+    # def _compute_attachment_ids(self):
+    #     for order in self:
+    #         soID = self._default_sol().order_id
+    #         attachment_idss = self.env['ir.attachment'].search([('res_id', '=', soID.id), ('res_model', '=', 'sale.order')]).ids
+    #         message_attachment_ids = soID.mapped('message_ids.attachment_ids').ids  # from mail_thread
+    #         ids = list(set(attachment_idss) - set(message_attachment_ids))
+    #         attachmentObj = self.env['ir.attachment'].browse(ids)
+    #         self.attachment_ids = attachmentObj
+    #         return attachmentObj
 
     def action_create_proof(self):
         self.ensure_one()
         self.check_active_proof()
         if self.sale_line_id:
-
-
-
             proofing_obj = self.env['sale.tmg_proofing']
             proof = proofing_obj.create({
                 'sale_line': self.sale_line_id.id,
-
                 'art_file': self.art_file.id,
                 'state': 'pending',
-
             })
             proof.send_proof()
         return {'type': 'ir.actions.act_window_close'}
