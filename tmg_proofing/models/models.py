ab6e28576f69acc80e3e4b3ed344ca33231f070c
ChristianDunn <cdunn@magnetllc.com>
2021-06-17 15:19:32 -0500

    Changed to binary data returned

--- tmg_proofing/models/models.py
+++ tmg_proofing/models/models.py
@@ -63,7 +63,7 @@ class tmg_proofing(models.Model):
         proof_ele = ET.Element('Proof')
 
 
-        artFile = ET.SubElement(proof_ele, "ProofLink").text = self.art_file.url
+        artFile = ET.SubElement(proof_ele, "ProofLink").text = self.art_file.datas
         # artFile.text = self.art_file.url
         saleOrderID = ET.SubElement(proof_ele, "SaleOrderID").text = str(self.sale_order.id)
         # saleOrderID.text = self.sale_order.id
