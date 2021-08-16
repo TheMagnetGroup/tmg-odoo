8a6c93aa523884119d1ce0b1e1a64231e93968e6
ChristianDunn <cdunn@magnetllc.com>
2021-08-16 08:12:27 -0500

    Changes to proof xml

--- tmg_proofing/models/models.py
+++ tmg_proofing/models/models.py
@@ -90,7 +90,7 @@ class tmg_proofing(models.Model):
         emails = ET.SubElement(proof_ele,"EmailTo").text = emails.replace(',',';')
         saleLineID = ET.SubElement(proof_ele, "SaleLineID").text = str(self.sale_line.id)
         description = ET.SubElement(proof_ele, "Description").text = self.sale_line.name
-        productionOrder = ET.SubElement(proof_ele, "WorkOrder").text = str(self.work_order)
+        productionOrder = ET.SubElement(proof_ele, "WorkOrder").text = str(self.work_order.name)
         suggestedID = ET.SubElement(proof_ele, "Suggested").text = str(self.suggested_layout)
         PONumber = ET.SubElement(proof_ele, "PONumber").text = self.sale_order.client_order_ref or ''
         Quantity = ET.SubElement(proof_ele, "Quantity").text = str(self.sale_line.product_uom_qty)
